c731bf52af54e2c507dc5cc7ca65ee6af19eba90
Angie <lewisangelique.c@gmail.com>
2026-02-12 12:47:18 -0500

    Updated websites

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -6,6 +6,12 @@ module.exports = {
 		frequency: 1, // (in minutes)
 	},
 	urls: [
-		"https://www.speedlify.dev/"
+		"https://www.ny.gov/",
+		"https://www.governor.ny.gov/",
+		"https://environmentalbondact.ny.gov/",
+		"https://digital-service-analytics.netlify.app/",
+		"https://digital.ny.gov/",
+		"https://omh.ny.gov/omhweb/crisis/988.html?utm_medium=301&utm_source=988.ny.gov",
+		"https://online2.ogs.ny.gov/greencleaning/"
 	]
 };
