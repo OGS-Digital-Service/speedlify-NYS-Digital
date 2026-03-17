fc2f681f7685c2bbb3cb189417d1ee91cdec3319
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2026-03-17 13:41:32 -0400

    Co-work to import components
    cc @AngeliqueLewis

--- _includes/findServices/_findServices.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const findServices = document.getElementById('nysds-find-services-highlight-id');
-    const container = findServices.closest(".o-wysiwyg");
-    if (container) {
-        container.style.maxWidth = '100%';
-        container.style.padding = "0px";
-    }
-})
--- _includes/translationDisclaimer/_append.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var main = document.querySelector('main');
-    main.setAttribute('lang', 'es');
-}, false);
